863ba4a7f949738f1cdf027a1a83c6dbddec8a65
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-03-04 17:56:57 +0530

    dropped idea of making it responsive

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const container = document.querySelector("#board");
-let div;
-for (let i = 1; i <= 16 * 16; i++) {
+let div, row, col;
+col = 16*2; //32
+row = 9*2;  //18
+
+for (let i = 1; i <= row * col; i++) {
   div = document.createElement("div");
-  div.style.width = "3.5dvw";
-  div.style.height = "3.5dvh";
+  div.style.width = "min(16px, 3.5dvw)";
+  div.style.height = "min(16px, 3.5dvh)";
   div.style.backgroundColor = "#F5E9CF";
+
+
   container.appendChild(div);
 }
+container.style.gridTemplateColumns = `repeat(${col}, 1fr`;
