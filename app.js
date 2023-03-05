328c442b4c89e69b59b50e4a63f7460c581bac49
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-03-05 17:00:30 +0530

    layout done. almost

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ row = 9*2;  //18
 
 for (let i = 1; i <= row * col; i++) {
   div = document.createElement("div");
-  div.style.width = "min(16px, 3.5dvw)";
-  div.style.height = "min(16px, 3.5dvh)";
+  
+  div.style.width = "1rem";
+  div.style.height = "1rem";
+
   div.style.backgroundColor = "#F5E9CF";
 
 
---
+++ bruh.js
@@ -0,0 +1,9 @@
+let a = 4;
+let b = 5;
+
+const sum = (a,b) => {
+
+  console.log(`sum is ${(a+b) + (b=6)}`);
+}
+
+console.log(sum(a,b));
