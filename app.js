2fa141ea612810ee4fcd45478c08dbb0cc6c8e22
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-03-08 22:34:34 +0530

    many changes

--- app.js
+++ app.js
@@ -2,58 +2,38 @@ const container = document.querySelector("#board");
 const phone = window.matchMedia("only screen and (max-width : 600px)");
 
 let div, row, col;
-var density = 0;
-let pixels = {
-  1: 2.52,
-  2: 1.19,
-  3: 0.746,
-  4: 0.526,
-};
 
-let pixelsForPhone = {
-  1: 1.7,
-  2: .787,
-  3: 0.482,
-  4: 0.331,
-};
+let density = 0;
 
-if(phone.matches) {
-  pixels = pixelsForPhone;
-}
-
-function changeDensity(pixels) {
+function changeDensity() {
   function deleteOldGrid() {
     document
       .querySelectorAll(".grid-item")
       .forEach((e) => e.parentNode.removeChild(e));
   }
   deleteOldGrid();
-
-  if (density < 4) {
-    return pixels[++density];
-  } else {
-    density = 1;
-    return pixels[density];
-  }
+  return density <= 4 ? ++density : (density = 1);
 }
 
 function makeGrid() {
-  let gridSize = changeDensity(pixels);
+  density = changeDensity();
+
   col = phone.matches ? 9 * density : 16 * density; //18 for phone else 32
   row = phone.matches ? 16 * density : 9 * density; //32 for phone else 18
 
+  container.style.gridTemplateColumns = `repeat(${col}, 1fr`;
+  // container.style.gridTemplateColumns = `repeat(${row}, 1fr`;
+
   for (let i = 1; i <= row * col; i++) {
     div = document.createElement("div");
-
-    div.style.width = `${gridSize}rem`;
-    div.style.height = `${gridSize}rem`;
+    div.style.width = "1em";
+    div.style.height = "1em";
 
     div.style.backgroundColor = "#F5E9CF";
     div.classList.add("grid-item");
 
     container.appendChild(div);
   }
-  container.style.gridTemplateColumns = `repeat(${col}, 1fr`;
 }
 
 makeGrid();
--- bruh.js
+++
@@ -1,9 +0,0 @@
-let a = 4;
-let b = 5;
-
-const sum = (a,b) => {
-
-  console.log(`sum is ${(a+b) + (b=6)}`);
-}
-
-console.log(sum(a,b));
