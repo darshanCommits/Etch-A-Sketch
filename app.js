99707d86c2ac06f1b4ad4f48ced5f8beaf0cec19
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-03-12 14:13:49 +0530

    QOL changess

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ let item, row, col, density;
 const container = document.querySelector(".container");
 const phone = window.matchMedia("only screen and (max-width : 600px)");
 
+//functions to manage setting up grid
+
+//    delets old grid
 function deleteOldGrid() {
   container.innerHTML = "";
 }
 
+//    makes new grid
 function makeGrid() {
   deleteOldGrid();
 
@@ -30,6 +34,14 @@ function makeGrid() {
 
 makeGrid();
 
+//functions to manage change in color
+
+//  creates random number to be used in rgb/hsl values
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//  main function to change color
 function changeColor(color) {
   addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("grid-item")) {
@@ -38,44 +50,21 @@ function changeColor(color) {
   });
 }
 
-function random(i) {
-  return Math.floor(Math.random() * i);
-}
-
-let intervalID;
+//variables for checking which color to use
+let isBW = true;
 let isLGBT = false;
-let isBW = false;
 let isEraser = false;
 
-function hoverAction(e) {
-  clearInterval(intervalID);
-
-  if (isLGBT) {
-    intervalID = setInterval(() => {
-      console.log(1);
-      let color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-      changeColor(color);
-    }, 100);
-  } else if (isBW) {
-    intervalID = setInterval(() => {
-      console.log(2);
-      let color = `hsl(0, 0%, ${random(100)}%)`;
-      changeColor(color);
-    }, 100);
-  } else if (isEraser) {
-    changeColor("white");
-  }
-}
-
+//event selector to set color
 function selectFunctionality(event) {
-  isLGBT = false;
   isBW = false;
+  isLGBT = false;
   isEraser = false;
 
-  if (event.target.classList.contains("lgbt")) {
-    isLGBT = true;
-  } else if (event.target.classList.contains("bw")) {
+  if (event.target.classList.contains("bw")) {
     isBW = true;
+  } else if (event.target.classList.contains("lgbt")) {
+    isLGBT = true;
   } else if (event.target.classList.contains("eraser")) {
     isEraser = true;
   }
@@ -85,15 +74,52 @@ document.querySelectorAll(".right button").forEach((button) => {
   button.addEventListener("click", selectFunctionality);
 });
 
+//function to manage drawing state
+
+let intervalID;
+
+function hoverAction(e) {
+  clearInterval(intervalID);
+
+  if (isLGBT) {
+    intervalID = setInterval(() => {
+      console.log(1);
+      let color = `hsl(${random(0, 360)}, ${random(80, 100)}%, ${random(60, 80)}%)`;
+      changeColor(color);
+    }, 250);
+  } else if (isBW) {
+    intervalID = setInterval(() => {
+      let color = `hsl(0, 0%, ${random(20, 60)}%)`;
+      console.log(color);
+      changeColor(color);
+    }, 250);
+  } else if (isEraser) {
+    changeColor("white");
+  }
+}
+
+//eventListeners to manage drawing state
+
+container.addEventListener("mouseenter", hoverAction);
+
+container.addEventListener("mouseleave", () => {
+  clearInterval(intervalID);
+});
+
+//Options
+
+//  reset the grid
 document.querySelector(".reset").addEventListener("mousedown", () => {
   density = 0;
   makeGrid();
 });
 
+//  change density
 document.querySelector(".density").addEventListener("mousedown", () => {
   makeGrid();
 });
 
+//  download the drawing in jpg format
 document.querySelector(".save").addEventListener("mousedown", () => {
   html2canvas(container).then(function (canvas) {
     const link = document.createElement("a");
@@ -104,10 +130,3 @@ document.querySelector(".save").addEventListener("mousedown", () => {
     link.click();
   });
 });
-
-container.addEventListener("mouseenter", hoverAction);
-
-container.addEventListener("mouseleave", () => {
-  clearInterval(intervalID);
-});
-
